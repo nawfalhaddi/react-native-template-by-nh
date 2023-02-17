2e9fa663d13fb12242fbb754fbc7de7d2d7179ac
nawfalhaddi <nawfal.haddi@tradeling.com>
2023-02-17 21:52:29 +0400

    Revert "feat: implement storybook"
    This reverts commit 95cfabdbf9ebe9ee58a265003b026e8dd78750f9.

--- template/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../src/ui/components/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-react-native-web',
-  ],
-  framework: '@storybook/react',
-};
--- template/metro.config.js
+++ template/metro.config.js
@@ -14,7 +14,4 @@ module.exports = {
       },
     }),
   },
-  resolver: {
-    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-  },
 };
